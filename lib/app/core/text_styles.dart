82705b204c252663f049a44673cb0e2f8d8f577a
Mateusz Kasprzak <mateusz.kasprzak17@gmail.com>
2024-05-14 10:51:31 +0200

    Create textstyles file

---
+++ lib/app/core/text_styles.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle titleTextStyle = GoogleFonts.lato(
+  fontSize: 25,
+  fontWeight: FontWeight.bold,
+  color: Colors.white,
+);
+
+TextStyle bodyTextStyle = GoogleFonts.lato(
+  fontSize: 15,
+  fontWeight: FontWeight.w600,
+  color: Colors.white,
+);
+
+TextStyle dateTextStyle = GoogleFonts.lato(
+  fontSize: 17,
+  fontWeight: FontWeight.w500,
+  color: Colors.grey,
+);
+
+TextStyle noteTitleTextStyle = GoogleFonts.lato(
+  fontSize: 19,
+  fontWeight: FontWeight.w600,
+  color: Colors.black,
+);
