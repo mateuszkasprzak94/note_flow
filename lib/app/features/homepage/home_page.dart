88e740ff2aa8c4462b8bec8cc57d9b5d51999a71
Mateusz Kasprzak <mateusz.kasprzak17@gmail.com>
2024-06-05 22:06:02 +0200

    Move searchbar widget to seperate file

--- lib/app/features/homepage/home_page.dart
+++ lib/app/features/homepage/home_page.dart
@@ -6,6 +6,7 @@ import 'package:note_flow/app/core/enums.dart';
 import 'package:note_flow/app/features/homepage/cubit/home_cubit.dart';
 import 'package:note_flow/app/features/homepage/widgets/floating_button.dart';
 import 'package:note_flow/app/features/homepage/widgets/home_page_body.dart';
+import 'package:note_flow/app/features/homepage/widgets/searchbar_widget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({
@@ -39,54 +40,6 @@ class _HomePageState extends State<HomePage> {
                 color: Colors.white, fontWeight: FontWeight.bold, fontSize: 35),
           ),
           centerTitle: true,
-          bottom: PreferredSize(
-            preferredSize: const Size.fromHeight(60),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: TextFormField(
-                style: const TextStyle(color: Colors.black),
-                controller: _controller,
-                onChanged: (query) {
-                  context.read<HomeCubit>().searchNotes(query);
-                },
-                maxLines: 1,
-                decoration: InputDecoration(
-                  contentPadding: const EdgeInsets.all(5),
-                  fillColor: Colors.white,
-                  filled: true,
-                  prefixIcon: const Icon(
-                    Icons.search,
-                    color: Colors.black,
-                  ),
-                  suffixIcon: _controller.text.isEmpty
-                      ? Container(width: 0)
-                      : IconButton(
-                          onPressed: () {
-                            _controller.clear();
-                            context.read<HomeCubit>().start();
-                          },
-                          icon: const Icon(
-                            Icons.close,
-                            color: Colors.black,
-                          ),
-                        ),
-                  hintText: 'Search your notes',
-                  hintStyle: const TextStyle(
-                    color: Colors.black87,
-                  ),
-                  border: const OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.white,
-                      width: 0.75,
-                    ),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(25),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
         ),
         floatingActionButton: const FloatingButton(),
         body: Container(
@@ -97,60 +50,65 @@ class _HomePageState extends State<HomePage> {
               end: Alignment.bottomRight,
             ),
           ),
-          child: BlocBuilder<HomeCubit, HomeState>(
-            builder: (context, state) {
-              final pinnedNotes = state.pinnedNotes;
-              final otherNotes = state.otherNotes;
-              final notes = state.items;
-              final errorMessage = state.errorMessage ?? 'Unknown error';
-              if (state.status == Status.error) {
-                return Center(
-                  child: Text(errorMessage),
-                );
-              }
+          child: Column(
+            children: [
+              SearchBarWidget(controller: _controller),
+              Expanded(
+                child: BlocBuilder<HomeCubit, HomeState>(
+                  builder: (context, state) {
+                    final pinnedNotes = state.pinnedNotes;
+                    final otherNotes = state.otherNotes;
+                    final notes = state.items;
+                    final errorMessage = state.errorMessage ?? 'Unknown error';
+                    if (state.status == Status.error) {
+                      return Center(
+                        child: Text(errorMessage),
+                      );
+                    }
 
-              if (state.status == Status.loading) {
-                return const Center(
-                  child: CircularProgressIndicator(
-                    color: kPrimaryIcon,
-                  ),
-                );
-              }
+                    if (state.status == Status.loading) {
+                      return const Center(
+                        child: CircularProgressIndicator(
+                          color: kPrimaryIcon,
+                        ),
+                      );
+                    }
 
-              if (notes.isEmpty) {
-                return const Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.note_add,
-                        color: kPrimaryIcon,
-                        size: 50,
-                      ),
-                      SizedBox(height: 15),
-                      Text(
-                        'Your Notes List is Empty',
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white),
-                      ),
-                    ],
-                  ),
-                );
-              }
+                    if (notes.isEmpty) {
+                      return const Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.note_add,
+                              color: kPrimaryIcon,
+                              size: 50,
+                            ),
+                            SizedBox(height: 15),
+                            Text(
+                              'Your Notes List is Empty',
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      );
+                    }
 
-              if (state.status == Status.success) {
-                return HomePageBodyWidget(
-                    pinnedNotes: pinnedNotes, otherNotes: otherNotes);
-              }
-              return const SizedBox.shrink();
-            },
+                    if (state.status == Status.success) {
+                      return HomePageBodyWidget(
+                          pinnedNotes: pinnedNotes, otherNotes: otherNotes);
+                    }
+                    return const SizedBox.shrink();
+                  },
+                ),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 }
-
-final controller = TextEditingController();
--- lib/app/features/homepage/widgets/home_page_body.dart
+++ lib/app/features/homepage/widgets/home_page_body.dart
@@ -8,7 +8,7 @@ import 'package:staggered_grid_view_flutter/widgets/staggered_grid_view.dart';
 import 'package:staggered_grid_view_flutter/widgets/staggered_tile.dart';
 
 class HomePageBodyWidget extends StatefulWidget {
-  HomePageBodyWidget({
+  const HomePageBodyWidget({
     super.key,
     required this.pinnedNotes,
     required this.otherNotes,
@@ -29,231 +29,146 @@ class _HomePageBodyWidgetState extends State<HomePageBodyWidget> {
     _controller.addListener(() => setState(() {}));
 
     return Scrollbar(
-      child: Column(
-        children: [
-          // Stack(
-          //   children: [
-          //     Container(
-          //       color: Colors.transparent,
-          //       width: double.infinity,
-          //       height: 75,
-          //     ),
-          //     Container(
-          //       decoration: const BoxDecoration(
-          //         borderRadius: BorderRadius.only(
-          //           bottomLeft: Radius.circular(25),
-          //           bottomRight: Radius.circular(25),
-          //         ),
-          //         color: Color(0xFF63686B),
-          //       ),
-          //       width: double.infinity,
-          //       height: 45,
-          //     ),
-          //     Positioned(
-          //       top: 20,
-          //       left: 0,
-          //       right: 0,
-          //       child: Padding(
-          //         padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          //         child: TextFormField(
-          //           style: const TextStyle(color: Colors.black),
-          //           controller: _controller,
-          //           onChanged: (query) {
-          //             context.read<HomeCubit>().searchNotes(query);
-          //           },
-          //           maxLines: 1,
-          //           decoration: InputDecoration(
-          //             contentPadding: const EdgeInsets.all(5),
-          //             fillColor: Colors.white,
-          //             filled: true,
-          //             prefixIcon: const Icon(
-          //               Icons.search,
-          //               color: Colors.black,
-          //             ),
-          //             suffixIcon: _controller.text.isEmpty
-          //                 ? Container(width: 0)
-          //                 : IconButton(
-          //                     onPressed: () => _controller.clear(),
-          //                     icon: const Icon(
-          //                       Icons.close,
-          //                       color: Colors.black,
-          //                     ),
-          //                   ),
-          //             hintText: 'Search your notes',
-          //             hintStyle: const TextStyle(
-          //               color: Colors.black87,
-          //             ),
-          //             border: const OutlineInputBorder(
-          //               borderSide: BorderSide(
-          //                 color: Colors.white,
-          //                 width: 0.75,
-          //               ),
-          //               borderRadius: BorderRadius.all(
-          //                 Radius.circular(25),
-          //               ),
-          //             ),
-          //           ),
-          //         ),
-          //       ),
-          //     ),
-          //   ],
-          // ),
-          Expanded(
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.only(bottom: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    if (widget.pinnedNotes.isNotEmpty) ...[
-                      const Padding(
-                        padding:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                        child: Text(
-                          'Pinned',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 5),
-                        child: StaggeredGridView.countBuilder(
-                          shrinkWrap: true,
-                          physics: const NeverScrollableScrollPhysics(),
-                          itemCount: widget.pinnedNotes.length,
-                          crossAxisCount: 2,
-                          crossAxisSpacing: 2,
-                          mainAxisSpacing: 2,
-                          staggeredTileBuilder: (index) =>
-                              const StaggeredTile.fit(1),
-                          itemBuilder: (context, index) {
-                            final note = widget.pinnedNotes[index];
-                            return NoteWidget(
-                              noteModel: note,
-                              onTap: () {
-                                Navigator.of(context).push(
-                                  MaterialPageRoute(
-                                    builder: (context) => AddNotePage(
-                                      noteModel: note,
-                                    ),
-                                  ),
-                                );
-                              },
-                              onLongPress: () {
-                                showDialog(
-                                  context: context,
-                                  builder: (context) {
-                                    return AlertDialog(
-                                      title: const Text(
-                                          'Are you sure you want to delete this note?'),
-                                      actions: [
-                                        ElevatedButton(
-                                          onPressed: () {
-                                            context
-                                                .read<HomeCubit>()
-                                                .delete(note);
-                                            Navigator.pop(context);
-                                          },
-                                          child: const Text('Yes'),
-                                        ),
-                                        ElevatedButton(
-                                          onPressed: () =>
-                                              Navigator.pop(context),
-                                          child: const Text('No'),
-                                        ),
-                                      ],
-                                    );
-                                  },
-                                );
-                              },
-                            );
-                          },
-                        ),
-                      ),
-                    ],
-                    if (widget.otherNotes.isNotEmpty &&
-                        widget.pinnedNotes.isNotEmpty)
-                      const Padding(
-                        padding:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                        child: Text(
-                          'Others',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    if (widget.otherNotes.isNotEmpty &&
-                        widget.pinnedNotes.isEmpty)
-                      const SizedBox(height: 20),
-                    if (widget.otherNotes.isNotEmpty) ...[
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 5),
-                        child: StaggeredGridView.countBuilder(
-                          shrinkWrap: true,
-                          physics: const NeverScrollableScrollPhysics(),
-                          itemCount: widget.otherNotes.length,
-                          crossAxisCount: 2,
-                          crossAxisSpacing: 2,
-                          mainAxisSpacing: 2,
-                          staggeredTileBuilder: (index) =>
-                              const StaggeredTile.fit(1),
-                          itemBuilder: (context, index) {
-                            final note = widget.otherNotes[index];
-                            return NoteWidget(
-                              noteModel: note,
-                              onTap: () async {
-                                Navigator.of(context).push(
-                                  MaterialPageRoute(
-                                    builder: (context) => AddNotePage(
-                                      noteModel: note,
-                                    ),
-                                  ),
-                                );
-                              },
-                              onLongPress: () async {
-                                showDialog(
-                                  context: context,
-                                  builder: (context) {
-                                    return AlertDialog(
-                                      title: const Text(
-                                          'Are you sure you want to delete this note?'),
-                                      actions: [
-                                        ElevatedButton(
-                                          onPressed: () async {
-                                            context
-                                                .read<HomeCubit>()
-                                                .delete(note);
-                                            Navigator.pop(context);
-                                          },
-                                          child: const Text('Yes'),
-                                        ),
-                                        ElevatedButton(
-                                          onPressed: () =>
-                                              Navigator.pop(context),
-                                          child: const Text('No'),
-                                        ),
-                                      ],
-                                    );
-                                  },
-                                );
-                              },
-                            );
-                          },
-                        ),
-                      ),
-                    ],
-                  ],
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              if (widget.pinnedNotes.isNotEmpty) ...[
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  child: Text(
+                    'Pinned',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
-              ),
-            ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 5),
+                  child: StaggeredGridView.countBuilder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: widget.pinnedNotes.length,
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 2,
+                    mainAxisSpacing: 2,
+                    staggeredTileBuilder: (index) => const StaggeredTile.fit(1),
+                    itemBuilder: (context, index) {
+                      final note = widget.pinnedNotes[index];
+                      return NoteWidget(
+                        noteModel: note,
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => AddNotePage(
+                                noteModel: note,
+                              ),
+                            ),
+                          );
+                        },
+                        onLongPress: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: const Text(
+                                    'Are you sure you want to delete this note?'),
+                                actions: [
+                                  ElevatedButton(
+                                    onPressed: () {
+                                      context.read<HomeCubit>().delete(note);
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text('Yes'),
+                                  ),
+                                  ElevatedButton(
+                                    onPressed: () => Navigator.pop(context),
+                                    child: const Text('No'),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ],
+              if (widget.otherNotes.isNotEmpty && widget.pinnedNotes.isNotEmpty)
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  child: Text(
+                    'Others',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              if (widget.otherNotes.isNotEmpty && widget.pinnedNotes.isEmpty)
+                const SizedBox(height: 20),
+              if (widget.otherNotes.isNotEmpty) ...[
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 5),
+                  child: StaggeredGridView.countBuilder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: widget.otherNotes.length,
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 2,
+                    mainAxisSpacing: 2,
+                    staggeredTileBuilder: (index) => const StaggeredTile.fit(1),
+                    itemBuilder: (context, index) {
+                      final note = widget.otherNotes[index];
+                      return NoteWidget(
+                        noteModel: note,
+                        onTap: () async {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => AddNotePage(
+                                noteModel: note,
+                              ),
+                            ),
+                          );
+                        },
+                        onLongPress: () async {
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: const Text(
+                                    'Are you sure you want to delete this note?'),
+                                actions: [
+                                  ElevatedButton(
+                                    onPressed: () async {
+                                      context.read<HomeCubit>().delete(note);
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text('Yes'),
+                                  ),
+                                  ElevatedButton(
+                                    onPressed: () => Navigator.pop(context),
+                                    child: const Text('No'),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
