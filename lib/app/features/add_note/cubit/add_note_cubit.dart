2885ac970ab331e0892394a1b448a4b8097fc7c6
Mateusz Kasprzak <mateusz.kasprzak17@gmail.com>
2024-06-06 16:13:27 +0200

    Inject repository in AddNoteCubit

--- lib/app/features/add_note/cubit/add_note_cubit.dart
+++ lib/app/features/add_note/cubit/add_note_cubit.dart
@@ -2,25 +2,25 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:note_flow/app/core/enums.dart';
-import 'package:note_flow/app/domain/database/db_helper.dart';
 import 'package:note_flow/app/domain/models/note_model.dart';
+import 'package:note_flow/app/domain/repository/notes_repository.dart';
 
 part 'add_note_state.dart';
 part 'add_note_cubit.freezed.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
-  AddNoteCubit(this.dbHelper) : super(AddNoteState());
+  AddNoteCubit(this._noteRepository) : super(AddNoteState());
 
-  final DBHelper dbHelper;
+  final NoteRepository _noteRepository;
 
   Future<void> addOrUpdate(NoteModel noteModel) async {
     try {
       if (noteModel.id == null) {
         // If the note ID is null, it means it's a new note
-        await dbHelper.addNote(noteModel);
+        await _noteRepository.addNote(noteModel);
       } else {
         // Otherwise, it's an existing note that needs to be updated
-        await dbHelper.updateNote(noteModel);
+        await _noteRepository.updateNote(noteModel);
       }
       emit(
         AddNoteState(
