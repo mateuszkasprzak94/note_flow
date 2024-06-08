4eb0e442266fac74652fd98ff8e3ecfe4340b2b5
Mateusz Kasprzak <mateusz.kasprzak17@gmail.com>
2024-06-08 18:26:14 +0200

    Remove padding

--- lib/app/features/add_note/widgets/note_textfield.widget.dart
+++ lib/app/features/add_note/widgets/note_textfield.widget.dart
@@ -10,50 +10,47 @@ class NoteTextFormFieldWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 20),
-      child: TextFormField(
-        style: const TextStyle(color: Colors.white),
-        controller: descriptionController,
-        maxLines: 5,
-        decoration: InputDecoration(
-          suffixIcon: descriptionController.text.isEmpty
-              ? Container(width: 0)
-              : IconButton(
-                  onPressed: () => descriptionController.clear(),
-                  icon: const Icon(
-                    Icons.clear,
-                    color: Colors.white,
-                  ),
+    return TextFormField(
+      style: const TextStyle(color: Colors.white),
+      controller: descriptionController,
+      maxLines: 5,
+      decoration: InputDecoration(
+        suffixIcon: descriptionController.text.isEmpty
+            ? Container(width: 0)
+            : IconButton(
+                onPressed: () => descriptionController.clear(),
+                icon: const Icon(
+                  Icons.clear,
+                  color: Colors.white,
                 ),
-          hintText: 'Type note here',
-          hintStyle: const TextStyle(
+              ),
+        hintText: 'Type note here',
+        hintStyle: const TextStyle(
+          color: Colors.white,
+        ),
+        labelText: 'Note',
+        labelStyle: const TextStyle(color: Colors.white),
+        border: const OutlineInputBorder(
+          borderSide: BorderSide(
             color: Colors.white,
+            width: 0.75,
           ),
-          labelText: 'Note',
-          labelStyle: const TextStyle(color: Colors.white),
-          border: const OutlineInputBorder(
-            borderSide: BorderSide(
-              color: Colors.white,
-              width: 0.75,
-            ),
-            borderRadius: BorderRadius.all(
-              Radius.circular(15),
-            ),
-          ),
-          enabledBorder: const OutlineInputBorder(
-            borderRadius: BorderRadius.all(
-              Radius.circular(15),
-            ),
-            borderSide: BorderSide(
-              color: Colors.white,
-              width: 1,
-            ),
+          borderRadius: BorderRadius.all(
+            Radius.circular(15),
           ),
         ),
-        keyboardType: TextInputType.multiline,
-        onChanged: (value) {},
+        enabledBorder: const OutlineInputBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(15),
+          ),
+          borderSide: BorderSide(
+            color: Colors.white,
+            width: 1,
+          ),
+        ),
       ),
+      keyboardType: TextInputType.multiline,
+      onChanged: (value) {},
     );
   }
 }
